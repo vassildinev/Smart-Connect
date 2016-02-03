2c85898792e48b251186dd87b7fb189fe9b761e6
V.Dinev <vassildinev@gmail.com>
2016-02-03 16:09:29 +0200

    Packages update and view engines configuration

---
+++ Source/Web/SmartConnect.Web.App/App_Start/ViewEnginesConfig.cs
@@ -0,0 +1,15 @@
+﻿namespace SmartConnect.Web.App
+{
+    using System.Web.Mvc;
+
+    public static class ViewEnginesConfig
+    {
+        public static void RegisterViewEngines(ViewEngineCollection viewEngines)
+        {
+            viewEngines.Clear();
+
+            var razor = new RazorViewEngine();
+            viewEngines.Add(razor);
+        }
+    }
+}
--- Source/Web/SmartConnect.Web.App/Global.asax.cs
+++ Source/Web/SmartConnect.Web.App/Global.asax.cs
@@ -1,18 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Optimization;
-using System.Web.Routing;
-
-namespace SmartConnect.Web.App
+﻿namespace SmartConnect.Web.App
 {
-    public class MvcApplication : System.Web.HttpApplication
+    using System.Web;
+    using System.Web.Mvc;
+    using System.Web.Optimization;
+    using System.Web.Routing;
+
+    public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            ViewEnginesConfig.RegisterViewEngines(ViewEngines.Engines);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
